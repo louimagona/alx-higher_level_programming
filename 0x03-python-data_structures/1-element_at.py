7556031fa7b5b9db9346c735fe999539a5bb9bea
louimagona <louimagona2@gmail.com>
2021-09-18 05:50:35 -0700

    refactored the code at index limits

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     list_len = len(my_list)
     if idx < 0:
         return(None)
-    elif idx > list_len:
+    elif idx >= list_len:
         return(None)
     else:
         return(my_list[idx])
