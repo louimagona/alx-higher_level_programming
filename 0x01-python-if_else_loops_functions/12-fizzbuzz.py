f1d8d0c699d75535d1227805ef98133f653d612d
louimagona <louimagona2@gmail.com>
2021-09-18 02:50:09 -0700

    changed the code to print FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 15 == 0:
-            i = "Fizzbuzz"
+            i = "FizzBuzz"
         elif i % 5 == 0:
             i = "Buzz"
         elif i % 3 == 0:
