480db10b0bd6d2f4a6942765624c79938b1986f2
louimagona <louimagona2@gmail.com>
2021-10-07 22:38:19 -0700

    created a function that computes weighted avearage

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+def weight_average(my_list=[]):
+    total = 0
+    no_items = 0
+    if not my_list:
+        return(0)
+    for i in my_list:
+        total += i[0] * i[1]
+        no_items += i[1]
+    return(total/no_items)
