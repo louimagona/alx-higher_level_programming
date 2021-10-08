a3287333c9f86f989054627556eb236a310a7938
louimagona <louimagona2@gmail.com>
2021-10-08 08:24:55 -0700

    added a test for value if it is an integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    try:
-        print("{:d}".format(int(value)))
-        return(True)
-    except ValueError:
-        return(False)
+    if isinstance(value, int):
+        try:
+            print("{:d}".format(value))
+            return(True)
+        except ValueError:
+            return(False)
